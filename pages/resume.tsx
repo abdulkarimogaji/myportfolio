f7780e232537d32da0a04a3b11d56d60b55fd7e5
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-06-01 21:55:13 +0100

    chore: complete resume page

---
+++ pages/resume.tsx
@@ -0,0 +1,74 @@
+const Resume = () => {
+  return (
+    <div className="resume">
+      <div className="box">
+        <div className="end">
+          <a href="" className="btn btn-download">
+            Download pdf
+          </a>
+        </div>
+
+        <div className="center">
+          <h2 style={{ marginBottom: "1.4rem" }}>Abdulkarim Ogaji</h2>
+          <a className="links">+2348166629550</a>
+          <a className="links">Email</a>
+          <a className="links">LinkedIn</a>
+          <a className="links">Github</a>
+          <a className="links">Abdulkarim</a>
+          <a className="links">Ogaji</a>
+        </div>
+        <h3 style={{ marginTop: "4rem" }}>Work Experience</h3>
+        <div>
+          <h4>
+            React Native Developer Intern at UIPRO | 3rd May - 5th August (2021)
+            | Certificate
+          </h4>
+          <ul>
+            <li>
+              Made an Android application of an e-commerce app from scratch
+              using React Native, an e-commerce application which also allows
+              users to customize their product.
+            </li>
+            <li>
+              Worked on a CRM application (A retailer and customer side
+              application which allows users to directly interact with retailers
+              for the service of the product), implemented and deployed to
+              production.
+            </li>
+          </ul>
+          <h3>Technical Skills</h3>
+          <ul>
+            <li>Languages: Java, JavaScript</li>
+            <li>
+              Frameworks: React.js, React Native, Next.js, Node.js, Express.js,
+              Spring Boot
+            </li>
+            <li>
+              Frameworks: React.js, React Native, Next.js, Node.js, Express.js,
+              Spring Boot
+            </li>
+            <li>Familiar With: DBMS, Computer Networks, MySQL, Docker</li>
+          </ul>
+          <h3>Projects</h3>
+          <h4>
+            Finance Tracker | Hackathon at inOut7.0 | 27th Nov - 22nd Dec (2020)
+          </h4>
+          <ul>
+            <li>
+              An Android app that allows users to scan their bills and have all
+              of their purchases added to their daily expenses.
+            </li>
+
+            <li>
+              Techs Used : React Native, Node.js, mongoDB, Flask, Heroku,
+              Tensorflow, Cloudinary, Google Cloud Vision API
+            </li>
+            <li>Project Links : GitHub | Playstore</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
