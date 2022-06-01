75bc3a5f9bbe7668cacef90ecdba3e2a4810ea06
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-06-01 15:32:31 +0100

    style: complete home page and navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NavBar = () => {
+  const router = useRouter();
+  console.log(router.pathname);
+  return (
+    <header>
+      <div className="progress"></div>
+      <nav>
+        <Image src="/favicon.ico" width="20px" height="20px" alt="logo" />
+        <ul>
+          <li>
+            <Link href="" className={router.pathname == "/" ? "active" : ""}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/projects"
+              className={router.pathname == "/projects" ? "active" : ""}
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/links"
+              className={router.pathname == "/links" ? "active" : ""}
+            >
+              Links
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/resume"
+              className={router.pathname == "/resume" ? "active" : ""}
+            >
+              Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
