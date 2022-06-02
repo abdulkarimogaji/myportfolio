6d3b9a763b5ca6026814283c936807f15a616e87
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-06-02 14:38:54 +0100

    style: activate all links

--- components/Project.tsx
+++ components/Project.tsx
@@ -15,14 +15,25 @@ const Project = ({ data }: Props) => {
   return (
     <>
       <div className="projects--image">
-        <Image src="/automart.png" alt="automart" layout="fill" className="contain" />
+        <Image
+          src="/automart.png"
+          alt="automart"
+          layout="fill"
+          className="contain"
+        />
       </div>
       <div className="project--details">
         <h2>{data.title}</h2>
         <p className="content">{data.description}</p>
         <div>
           {data.links.map((link) => (
-            <a className="btn pill projects--a" href={link.url} key={link.text}>
+            <a
+              className="btn pill projects--a"
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+              key={link.text}
+            >
               {" "}
               {link.text}
             </a>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,15 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import NavBar from "../components/NavBar";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <Head>
+        <title>Abdulkarim Ogaji</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </>
